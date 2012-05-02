87c8866a39486238866019ad44180b5a0bf23f22
Dane Bratz <danebratz@gmail.com>
2012-05-02 16:58:29 -0700

    newest

--- src/player/gamer/statemachine/simple/ABSimpleGamer.java
+++ src/player/gamer/statemachine/simple/ABSimpleGamer.java
@@ -28,6 +28,7 @@ public final class ABSimpleGamer extends StateMachineGamer {
 	private static final int GOAL_MAX = 100;
 	private static final int GOAL_MIN = 0;
 	private static final int EVAL_FN_MAX = 80;
+	private static final int NO_SOLUTION = -1;
 	private double max_search_mobility; //use this in metagame computation of relative max for heuristics
 	private long finishBy;
 	
@@ -56,24 +57,31 @@ public final class ABSimpleGamer extends StateMachineGamer {
 	 */
 	private int monteCarloDepthSearch(long timeout) throws TransitionDefinitionException, MoveDefinitionException {  
 		long start = System.currentTimeMillis();
-		finishBy = start + timeout - 1000;
+		finishBy = timeout - 1000;
         ArrayList<Integer> maxList = new ArrayList<Integer>();
         while(true) {
         	MachineState state = getStateMachine().getInitialState();
-		    if (System.currentTimeMillis() > finishBy)
-		        break;
 		    while(!getStateMachine().isTerminal(state)) {
-		    	state = getStateMachine().getNextStateDestructively(state, getStateMachine().getRandomJointMove(state));
+		    	if (System.currentTimeMillis() > finishBy)
+		    		break;
+		    	state = getStateMachine().getNextState(state, getStateMachine().getRandomJointMove(state));
 		    	int localMax = 0;
 		    	for(int i = 0; i < getStateMachine().getRoles().size(); i++){
-		    		int temp = getStateMachine().getLegalMoves(state, getStateMachine().getRoles().get(i)).size();
+		    		int temp = 0;
+		    		try{
+		    			temp = getStateMachine().getLegalMoves(state, getStateMachine().getRoles().get(i)).size();
+		    		} catch (MoveDefinitionException e){
+		    			
+		    		}
 		    		if(temp >= localMax){
 		    			localMax = temp;
 		    		}
 		    	}
             		maxList.add(localMax);
         	}
-        }
+		    if (System.currentTimeMillis() > finishBy)
+	    		break;
+        }     
         return Collections.max(maxList);
     }    
 
@@ -82,23 +90,29 @@ public final class ABSimpleGamer extends StateMachineGamer {
 			throws TransitionDefinitionException, MoveDefinitionException,
 			GoalDefinitionException {
 		long start = System.currentTimeMillis();
-		finishBy = start + timeout - 1000;
+		finishBy = timeout - 1000;
 		StateMachine stateMachine = getStateMachine();
-		MachineState currentState = getCurrentState();
+		//MachineState currentState = getCurrentState();
 		Role role = getRole();
-		List<Move> moves = stateMachine.getLegalMoves(currentState, role);
+		List<Move> moves = stateMachine.getLegalMoves(getCurrentState(), role);
 		
 		int maxIndex = 0;
 		int maxValue = 0;
-		for(int i=0; i<moves.size(); i++){
-			int nextValue = getMinValue(currentState, role, moves.get(i), Integer.MIN_VALUE, Integer.MAX_VALUE, 0);
-			if(nextValue > maxValue){
-				maxValue = nextValue;
-				maxIndex = i;
+		int j = 0;
+		while(true){
+			for(int i=0; i<moves.size(); i++){
+				int nextValue = getMinValue(getCurrentState(), role, moves.get(i), Integer.MIN_VALUE, Integer.MAX_VALUE, j);
+				if(nextValue > maxValue){
+					maxValue = nextValue;
+					maxIndex = i;
+				}
+				if(System.currentTimeMillis() > finishBy) break;
 			}
+			if(System.currentTimeMillis() > finishBy) break;
+			j++;
 		}
-		System.out.print("Count = ");
-		System.out.println(count); //////////////
+		//System.out.print("Count = ");
+		//System.out.println(count); //////////////
 		return moves.get(maxIndex);
 	}
 
@@ -115,12 +129,10 @@ public final class ABSimpleGamer extends StateMachineGamer {
 	}
 	
 	private int getMaxValue(MachineState currentState, Role role, int alpha, int beta, int level) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException {
-		count ++; ////////
+		//count ++; ////////
 		StateMachine stateMachine = getStateMachine();
-		if(stateMachine.isTerminal(currentState))
-			return stateMachine.getGoal(currentState, role);
 		if(!expFn(currentState, level))
-			return evalFn(currentState, role);
+			return evalFn(currentState, role, level);
 		List<Move> moves = stateMachine.getLegalMoves(currentState, role);
 		
 		int maxValue = 0;
@@ -136,7 +148,7 @@ public final class ABSimpleGamer extends StateMachineGamer {
 	}
 	
 	private int getMinValue(MachineState currentState, Role role, Move move, int alpha, int beta, int level) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException {
-		count ++; //////////
+		//count ++; //////////
 		StateMachine stateMachine = getStateMachine();
 		List<List<Move>> jointMoves = stateMachine.getLegalJointMoves(currentState, role, move);
 		
@@ -146,12 +158,12 @@ public final class ABSimpleGamer extends StateMachineGamer {
 			int stateHashCode = newState.hashCode();
 			int nextValue;
 			if(!stateCodeMap.containsKey(stateHashCode)) {
-				nextValue = getMaxValue(newState, role, alpha, beta, level+1);
+				nextValue = getMaxValue(newState, role, alpha, beta, level-1);
 				stateCodeMap.put(stateHashCode,nextValue);
 			} else {
 				nextValue = stateCodeMap.get(stateHashCode);
 			}
-			if(nextValue < minValue)
+			if(nextValue < minValue && nextValue != NO_SOLUTION)
 				minValue = nextValue;
 			if(minValue <= alpha) return minValue;
 			if(minValue < beta) beta = minValue;
@@ -162,17 +174,39 @@ public final class ABSimpleGamer extends StateMachineGamer {
 	/*
 	 * Serves as wrapper for specific heuristic implementations
 	 */
-	private int evalFn(MachineState state, Role role) throws MoveDefinitionException{
-		return mobility1(state, role);
+	private int evalFn(MachineState state, Role role, int level) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException{
+		if(getStateMachine().isTerminal(state)){
+			//System.out.println("evalfn terminal state");
+			return getStateMachine().getGoal(state, role);
+		}
+		if(level <= 0) return NO_SOLUTION;
+		return mobility2(state, role);
 	}
 	
 	/*
 	 * As of now, simply does a depth check
 	 */
 	private boolean expFn(MachineState state, int level){
-		if(level > MAX_DEPTH) return false;
+		if(getStateMachine().isTerminal(state)) {
+			//System.out.println("expfn terminal state");
+			return false;
+		}
+		//if(level > MAX_DEPTH) return false;
 		if(System.currentTimeMillis() > finishBy) return false;
 		else return true;
+	}
+	
+	
+	/*
+	 * Simple Focus heuristic - depth of one
+	 */
+	private int focus1(MachineState state, Role role) throws MoveDefinitionException {
+		List<Move> moveList = getStateMachine().getLegalMoves(state,role);
+		double size = (double)moveList.size();
+		size = this.max_search_mobility - size;
+		int normalized = (int) ((size/this.max_search_mobility) * GOAL_MAX);
+		if(normalized <= GOAL_MIN) return EVAL_FN_MAX;
+		return normalized;
 	}
 	
 	
@@ -194,11 +228,15 @@ public final class ABSimpleGamer extends StateMachineGamer {
 		StateMachine stateMachine = getStateMachine();
 		List<List<Move>> moveList = stateMachine.getLegalJointMoves(state);
 		int mobility = moveList.size();
+		int count = 1;
 		for(List<Move> moves:moveList) {
 			MachineState next = stateMachine.getNextState(state, moves);
-			mobility += stateMachine.getLegalMoves(next,role).size();
+			try{
+				mobility += stateMachine.getLegalMoves(next,role).size();
+				count++;
+			} catch(MoveDefinitionException e){}
 		}
-		return mobility;
+		return mobility/count; //average mobilities coming from a state - could do max or min too
 	}
 	
 	/*
